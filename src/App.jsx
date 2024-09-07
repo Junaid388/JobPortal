6cb90df14dc3b4add3d94a3b7f5862ccde8342c1
Junaid388 <junaidkhan.5a4@gmail.com>
2024-09-07 20:32:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css'
+
+import { Button } from './components/ui/button';
+import AppLayout from './layouts/app-layout';
+import LandingPage from './pages/landing';
+
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />
+      }
+
+    ]
+  }
+])
+
+function App() {
+
+  return (
+    <RouterProvider router={router} />);
+}
+
+export default App
